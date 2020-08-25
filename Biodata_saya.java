c8391f089a47263bc136375deeed41a39581cfdc
nurizkiadel <70134526+nurizkiadel@users.noreply.github.com>
2020-08-25 20:50:35 +0700

    Add files via upload

---
+++ Biodata_saya.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata_saya;
+
+/**
+ *
+ * @author user Nurizki Adelia Azzahro
+ */
+public class Biodata_saya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String nama="Nurizki Adelia Azzahro";
+       String jk="perempuan";
+       String alamat="Ds.Dayurejo,Kec.Prigen";
+       String jurusan="XI RPL 1";
+      
+       System.out.println("biodata saya");//mengeprint atau mencetak
+       System.out.println("--------");
+       System.out.println("Nama \t\t\t:"+nama);
+       System.out.println("Jk \t\t\t:"+jk);
+       System.out.println("Alamat \t\t\t:"+alamat);
+       System.out.println("Jurusan \t\t:"+jurusan);
+    }
+    
+}
